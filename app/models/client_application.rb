cfd1f0fce485f71f4edf2eb22fb1df4312ea5d25
omar <omarmaboelenen@gmail.com>
2019-07-18 10:58:20 +0200

    Application Now does not return id

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -4,6 +4,15 @@ class ClientApplication < ApplicationRecord
   before_create :create_identifier_token
 
   def create_identifier_token
-    self.identifier_token=SecureRandom.urlsafe_base64(10)
+    self.identifier_token = SecureRandom.urlsafe_base64(10)
+  end
+
+  def as_json(options = {})
+    {
+        :name => name,
+        :identifier_token => identifier_token,
+        :created_at => created_at,
+        :updated_at => updated_at
+    }
   end
 end
--- spec/requests/client_applications_spec.rb
+++ spec/requests/client_applications_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "ClientApplications", type: :request do
       json_response_body = JSON.parse(response.body)
       expect(json_response_body["applications"]).to_not be_nil
       expect(json_response_body["applications"].length).to eq(2)
-      expect(json_response_body["applications"][0]["id"])
+      expect(json_response_body["applications"][0]["id"]).to be_nil
     end
   end
 end
